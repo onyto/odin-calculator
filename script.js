54ecacd28b8654c02ebe8583aca9ebf12ef14926
onyto <onyto@tutanota.com>
2022-03-29 16:30:23 +0200

    Make calculator display work with some keys
     - Keypresses on numbers, +, -, x and / show up on the display
     - CLR clears the display and DEL removes the last input

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const displayValue = document.getElementById('display-value');
+
+addKeyEvent();
+
 function add(a, b) {
   return a + b
 }
@@ -31,4 +35,56 @@ function operate(operator, a, b) {
     default:
       break;
   }
+}
+
+function addKeyEvent() {
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(key => key.addEventListener('click', keyEvent));
+}
+
+function keyEvent(e) {
+  const key = e.target;
+  const displayValueArray = displayValue.textContent.split("");
+
+  switch (key.textContent) {
+    case 'CLR':
+      updateDisplay([]);
+      break;
+
+    case 'DEL':
+      displayValueArray.pop();
+      updateDisplay(displayValueArray);
+      break;
+
+    case '=':
+      
+      break;
+
+    case '.':
+      // do nothing for now
+      break;
+  
+    default:
+      displayValueArray.push(key.textContent);
+      updateDisplay(displayValueArray);
+      break;
+  }
+
+  // On Equal call a function that looks at the current display and
+  // slice it to 3 parts(number, operator, number) or 2 parts(number, operator)
+  // and then call the correct math function.
+  // Put all of that in an overall reduce() that loops through the objects
+}
+
+function updateDisplay(values) {
+  if (values.length >= 1) {
+    displayValue.textContent = values.join("");
+  }
+  else {
+    displayValue.textContent = "";
+  }
+}
+
+function getDisplay() {
+  // return an array of the displayValue
 }
